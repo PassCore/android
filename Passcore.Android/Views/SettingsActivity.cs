c5e5c883c7585ab99d5860f9272c264d1d3ea439
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-04-14 21:19:18 +0100

    add reset

--- Passcore.Android/Views/SettingsActivity.cs
+++ Passcore.Android/Views/SettingsActivity.cs
@@ -1,8 +1,8 @@
-﻿using Android.App;
+﻿using System.IO;
+using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Widget;
-
 using Passcore.Android.Helper;
 
 namespace Passcore.Android.Views
@@ -11,11 +11,13 @@ namespace Passcore.Android.Views
     public class SettingsActivity : AppCompatActivity
     {
         Switch SwtBlockScreenshot,
-               SwtSaveEnhance,
-               SwtSavePassword,
-               SwtSaveMasterKey,
-               SwtSavePasswordLength,
-               SwtSaveIsCharRequired;
+            SwtSaveEnhance,
+            SwtSavePassword,
+            SwtSaveMasterKey,
+            SwtSavePasswordLength,
+            SwtSaveIsCharRequired;
+
+        Button BtnResetAll;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -31,6 +33,7 @@ namespace Passcore.Android.Views
             SwtSaveMasterKey = FindViewById<Switch>(Resource.Id.SwtSaveMasterKey);
             SwtSavePasswordLength = FindViewById<Switch>(Resource.Id.SwtSavePasswordLength);
             SwtSaveIsCharRequired = FindViewById<Switch>(Resource.Id.SwtSaveIsCharRequired);
+            BtnResetAll = FindViewById<Button>(Resource.Id.BtnResetAll);
 
             SwtSaveEnhance.Checked = Shared.Config.IsStoreEnhance;
             SwtSavePassword.Checked = Shared.Config.IsStorePassword;
@@ -38,9 +41,7 @@ namespace Passcore.Android.Views
             SwtSavePasswordLength.Checked = Shared.Config.IsStorePasswordLength;
             SwtSaveIsCharRequired.Checked = Shared.Config.IsStoreCharRequired;
 
-            SwtBlockScreenshot.Checked = Shared.MainActivity != null ?
-                Shared.MainActivity.IsSecure :
-                false;
+            SwtBlockScreenshot.Checked = Shared.MainActivity != null ? Shared.MainActivity.IsSecure : false;
 
             SwtBlockScreenshot.CheckedChange += SwtBlockScreenshot_CheckedChange;
             SwtSaveMasterKey.CheckedChange += SwtSaveMasterKey_CheckedChange;
@@ -48,6 +49,21 @@ namespace Passcore.Android.Views
             SwtSavePassword.CheckedChange += SwtSavePassword_CheckedChange;
             SwtSavePasswordLength.CheckedChange += SwtSavePasswordLength_CheckedChange;
             SwtSaveIsCharRequired.CheckedChange += SwtSaveIsCharRequired_CheckedChange;
+            BtnResetAll.Click += BtnResetAll_Click;
+        }
+
+        private void BtnResetAll_Click(object sender, System.EventArgs e)
+        {
+            var a = new global::Android.App.AlertDialog.Builder(this).Create();
+            a.SetTitle(Resources.GetString(Resource.String.caution));
+            a.SetMessage(Resources.GetString(Resource.String.reset_all_warn));
+            a.SetButton(Resources.GetString(Resource.String.ok), (s, a) =>
+            {
+                File.Delete(Path.Combine(IOHelper.GetDataPath(), "config.pc"));
+                System.Environment.Exit(0);
+            });
+            a.SetButton2(Resources.GetString(Resource.String.cancel), (s, a) => { });
+            a.Show();
         }
 
         private void SwtSaveIsCharRequired_CheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e)
